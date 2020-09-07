49b1791a48d450f8f1f3350780237107473d9d6a
hushun1214 <2375353463@qq.com>
2020-09-07 22:24:54 +0800

    完成icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import icon from "icons/money.svg";
+require("icons/money.svg");
+require("icons/tag.svg");
+require("icons/chart.svg");
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -11,10 +13,14 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.3333333%;
       text-align: center;
-      padding: 16px;
-      > img {
-        max-width: 32px;
-        max-height: 32px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        width: 24px;
+        height: 24px;
       }
     }
   }
@@ -25,13 +31,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <img src={icon} alt="" />
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart" />
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
