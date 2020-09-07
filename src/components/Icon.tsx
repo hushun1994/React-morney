1eac04f8d5265515240114d02a272ba8eb18bcec
hushun1214 <2375353463@qq.com>
2020-09-07 22:52:25 +0800

    创建 Icon 组件

---
+++ src/components/Icon.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => {
+  requireContext.keys().forEach(requireContext);
+};
+try {
+  importAll(require.context("icons", true, /\.svg$/));
+} catch (err) {
+  console.log(err);
+}
+
+type Props = {
+  name: string;
+};
+
+const Icon = (props: Props) => {
+  return (
+    <svg className="icon">
+      <use xlinkHref={"#" + props.name} />
+    </svg>
+  );
+};
+
+export default Icon;
